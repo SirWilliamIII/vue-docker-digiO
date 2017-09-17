ca860139545b5dd76bbcb1ab84f6de53aeb192b1
Will Carpenter <willcarpenteriii@gmail.com>
2017-09-17 15:46:37 -0600

    added vue resource and employees, alert, about components

---
+++ src/components/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about container">
+    <h1 class="page-header">About</h1>
+    <p>This is a employee management app built with Vue.js</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,49 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+import Employees from './components/Employees'
+import About from './components/About'
+
+const router = new VueRouter({
+	base:   __dirname,
+	routes: [
+		{ path: '/', component: Employees },
+		{ path: '/about', component: About }
+	]
+})
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
-})
+	router,
+	template: `
+    <div id="app">
+      <nav class="navbar navbar-default">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">vCustomers</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><router-link to="/">Home</router-link></li>
+            <li><router-link to="/about">About</router-link></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><router-link to="/add">Add Customer</router-link></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+      <router-view></router-view>
+    </div>
+  `
+}).$mount('#app')
